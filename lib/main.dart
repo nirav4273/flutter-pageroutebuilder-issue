fec29f4ae001d93f14f737054e923455be2d8433
nirav4273 <nirav4273@gmail.com>
2018-11-19 22:38:11 +0530

    Add code

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import "package:flutter/material.dart";
+void main(){
+  runApp(MainPage());
+}
+
+class MainPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "Login",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+          child: RaisedButton(
+              child: Text("Click"),
+              color: Colors.blue,
+              onPressed: (){
+                Navigator.of(context).push(PageRouteBuilder(
+                    opaque: false,
+                    barrierDismissible: true,
+                    barrierColor: Colors.red,
+                    pageBuilder: (context,_,__){
+                      return Center(
+                          child: Container(
+                            height: 200.0,
+                            width: 200.0,
+                            decoration: BoxDecoration(
+                                color: Colors.greenAccent,
+                                borderRadius: BorderRadius.circular(5.0)
+                            ),
+                          )
+                      );
+                    }
+                ));
+              }
+          )
+      ),
+    );
+  }
+}
